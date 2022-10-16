6d9ab1441ccab7443f55e69466899a4c3f67411d
Syauqi Yasman <syauqiyasman@gmail.com>
2022-10-16 07:48:52 +0700

    Make an eraser (#3)

--- src/components/ColorButton.tsx
+++ src/components/ColorButton.tsx
@@ -4,19 +4,24 @@ type ColorButtonProps = {
   color: string
   brushColor: string
   setBrushColor: React.Dispatch<React.SetStateAction<string>>
+  setIsEraser: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export default function ColorButton({ color, brushColor, setBrushColor }: ColorButtonProps) {
+export default function ColorButton({ color, brushColor, setBrushColor, setIsEraser }: ColorButtonProps) {
   const keyDownHandler = (e: { code: string }) => {
     if (e.code === 'Enter' || e.code === 'Space') {
       setBrushColor(color)
+      setIsEraser(false)
     }
   }
   return (
     <div
       className={styles.colorButtonOuter}
       style={{ backgroundColor: brushColor === color ? '#dadce0' : '' }}
-      onClick={() => setBrushColor(color)}
+      onClick={() => {
+        setBrushColor(color)
+        setIsEraser(false)
+      }}
       onKeyDown={keyDownHandler}
       tabIndex={0}
       role="button"
